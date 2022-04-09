92fe5b2099e4bb0adbb9feed8d48e7f4428791f8
shruthii <shruthii@users.noreply.github.com>
2022-04-09 19:20:53 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -14,3 +14,19 @@ else:
     
     
     #####palindrome
+    
+    
+    
+arr1='malayalamo'
+i,j=0,len(arr1)-1
+
+while i!=j:
+    if arr1[i] == arr1[j]:
+        i+=1
+        j-=1
+        continue
+    else:
+        print("not a palindrome")
+        break
+else:
+    print("Its a palindrome")
