e9b914cc9f0bcdc95ad38215ed317c61143b0a81
shruthii <shruthii@users.noreply.github.com>
2022-04-09 19:11:54 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+
+arr1= "malayalam"
+l2=[""]*len(arr1)
+i=-1
+for x in arr1:
+    l2[i]=x
+    i-=1
+arr2=''.join(l2)  
+if arr1 == arr2:
+    print('palindrome')
+else:
+    print('not')
+    
+    
+    
+    #####palindrome
